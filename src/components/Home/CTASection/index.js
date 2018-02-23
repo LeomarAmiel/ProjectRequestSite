973ffb1604e4ff927bc76ed546cdeb5d571dd4df
Leomar Alejar <leomaramieldev@gmail.com>
2018-02-23 17:25:09 +0800

    Fix padding for CTASection

--- src/components/Home/CTASection/index.js
+++ src/components/Home/CTASection/index.js
@@ -7,10 +7,13 @@ import GadgetSection from './gadgetSection';
 const Wrapper = styled.section`
     display: flex;
     justify-content: center;
+    padding-bottom: 6.2rem; 
     @media(max-width: 768px){
+        padding-bottom: 0;
         flex-wrap: wrap;
     }
     @media(min-width: 769px) and (max-width: 1200px){
+        padding-bottom: 1rem;
         justify-content: space-between;
     }
 `;
