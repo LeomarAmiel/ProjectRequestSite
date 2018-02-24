180febe352c75cbbbc63a65e366b97cf500ee1b6
Leomar Alejar <leomaramieldev@gmail.com>
2018-02-24 21:49:25 +0800

    Add WIP signup server function using Firestore

--- functions/controllers/index.js
+++ functions/controllers/index.js
@@ -5,21 +5,32 @@ exports.signup = function(req, res, next){
     const email = req.body.email;
     const password = req.body.password;
 
-    // if(!email||!password) {
-    //     return res.status(422).send({Error: 'Email and/or password is rquired'});
-    // }
-    // Will fetch list of users for front-end checking of email in form
-    // Adding server-checking for now
+    if(!email||!password) {
+        return res.status(422).send({Error: 'Email and/or password is rquired'});
+    }
+
+    var sameEmail = false;
     db.collection('users').get()
     .then((querySnapshot) => {
+        var forEachCounter = 0;
         querySnapshot.forEach((doc) => {
-            if(email!==doc.data().email){
-                db.collection('users').add({
-                    email,
-                    password
-                })
+            forEachCounter++;
+            if(email===doc.data().email){
+                console.log('if');
+                sameEmail= true;
+                return res.send({"Error": "Email is already in use"});
+            }
+            else{
+                if(forEachCounter === querySnapshot.size && sameEmail != true) {
+                    db.collection('users').add({
+                        email,
+                        password
+                    })
+                    .then(() => res.json())
+                    .catch(err => res.send(err));
+                }
             }
         })
     })
-    next();
+    .catch(err => res.send({"Error": err}))
 }
