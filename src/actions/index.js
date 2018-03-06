cc7c798638492f800de9ff086449ddc2dca6ccd0
Leomar Alejar <leomaramieldev@gmail.com>
2018-03-06 23:32:49 +0800

    Add WIP actions and reducers

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const TOGGLE_MODAL = 'TOGGLE_MODAL';
+
+export const toggleOpenModal = (payload) => ({
+    type: TOGGLE_MODAL,
+    payload
+});
---
+++ src/reducers/modalReducer.js
@@ -0,0 +1,20 @@
+import { TOGGLE_MODAL } from '../actions'
+
+const initialState = {
+    isShowingModal: false,
+    type: 'LOG_IN'
+};
+
+export default (state = initialState, action) => {
+    console.log(state);
+    console.log(action);
+    switch(action.type) {
+        case TOGGLE_MODAL:
+            return {
+                isShowingModal: state.isShowingModal ? false : true,
+                type: action.payload
+            };
+        default:
+            return state;
+    }
+}
