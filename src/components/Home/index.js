80e61efa24209584bbca81aedf625d49a68cc10f
Leomar Alejar <leomaramieldev@gmail.com>
2018-03-08 19:15:15 +0800

    Move header and footer to Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import CTASection from './CTASection';
 import StackSection from './StackSection';
 import DownloadAppSection from './DownloadAppSection';
+import Header from '../Header';
+import Footer from '../Footer';
 
 const Wrapper = styled.div`
     display: flex;
@@ -11,8 +13,10 @@ const Wrapper = styled.div`
 
 export default () => (
     <Wrapper>
+        <Header/>
         <CTASection/>
         <StackSection/>
         <DownloadAppSection/>
+        <Footer/>
     </Wrapper>
 )
