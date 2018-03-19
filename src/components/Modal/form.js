e4fdd40ec6da6cbc2777f404a2453c404aa2a404
Leomar Alejar <leomaramieldev@gmail.com>
2018-03-19 23:42:24 +0800

    Abstracted form for smaller re-renders

---
+++ src/components/Modal/form.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { signUp } from '../../actions';
+
+const FormWrapper = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 1.5rem 0;
+`;
+
+const Input = styled.input`
+    height: 45px;
+    width: 15rem;
+    border: 0;
+    border-bottom: 2px solid rgb(236, 236, 236);
+    font-size: .65rem;  
+    &:focus {
+        outline: none;
+        border-bottom: 2px solid rgb(42, 97, 24);
+        transition: border-bottom .3s cubic-bezier(0.4, 0, 0.2, 1);
+    }
+    &::placeholder{
+        color: rgb(180, 180, 180);
+        font-size: .65rem; 
+        font-weight: 300;
+    }
+`;
+
+
+const Button = styled.button`
+    background-color: rgb(42, 97, 24);
+    color: white;
+    border-radius: 30px;
+    padding: .65rem 1.25rem;
+    margin: .3rem 0;
+    font-size: .55rem;
+    font-weight: 700;
+    &:focus{
+        outline: none;
+    }
+`;
+
+const ButtonSeparator = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const TextSeparator = styled.span`
+    color: #979797;
+    font-size: .5rem;
+    font-weight: 100;
+`;
+
+const HR = styled.hr`
+    border: 0;
+    border-bottom: 1px solid #D7D7D7;
+    width: 45%;
+`;
+
+const FacebookButton = Button.extend`
+    background-color: rgb(60, 82, 136);
+`;
+
+class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    changeEmail (e) {
+        this.setState({ email: e.target.value});
+    }
+    changePassword (e) {
+        this.setState({ password: e.target.value});
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        // this.props.signUp({email: this.state.email, password: this.state.password}, this.props.onModalForm);
+    }
+
+    render(){
+        return (
+            <FormWrapper onSubmit={this.handleSubmit}>
+                <Input placeholder='Email address' value={this.state.email} onChange={this.changeEmail.bind(this)}/>
+                <Input placeholder='Password' value={this.state.password} onChange={this.changePassword.bind(this)}/>
+                {this.props.children}
+                <Button type="submit">
+                    { this.props.onModalForm==='login' ? 'SIGN IN': 'SIGN UP' }
+                </Button>
+                <ButtonSeparator>
+                    <HR/>
+                    <TextSeparator>
+                        or
+                    </TextSeparator>
+                    <HR/>
+                </ButtonSeparator>
+                <FacebookButton> FACEBOOK </FacebookButton>
+            </FormWrapper>
+        );
+    }
+}
+
+export default connect(null, { signUp })(Form)
--- src/components/Modal/modalSection.js
+++ src/components/Modal/modalSection.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { redirectModal, toggleModal } from '../../actions';
 import Header from '../Header';
+import Form from './form';
 
 const Wrapper = styled.div`
     display: flex;
@@ -32,30 +33,6 @@ const Heading = styled.h3`
     }
 `;
 
-const Input = styled.input`
-    height: 45px;
-    width: 15rem;
-    border: 0;
-    border-bottom: 2px solid rgb(236, 236, 236);
-    &:focus {
-        outline: none;
-        border-bottom: 2px solid rgb(42, 97, 24);
-        transition: border-bottom .3s cubic-bezier(0.4, 0, 0.2, 1);
-    }
-    &::placeholder{
-        color: rgb(180, 180, 180);
-        font-size: .65rem; 
-        font-weight: 300;
-    }
-`;
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin: 1.5rem 0;
-`;
-
 const ForgotPasswordLink = styled.a`
     font-size: .45rem;
     color: rgb(84, 135, 247);
@@ -65,41 +42,6 @@ const ForgotPasswordLink = styled.a`
 const TermsText = styled.span`
     font-size: .45rem;
     padding: .75rem 0;
-`;
-
-const Button = styled.button`
-    background-color: rgb(42, 97, 24);
-    color: white;
-    border-radius: 30px;
-    padding: .65rem 1.25rem;
-    margin: .3rem 0;
-    font-size: .55rem;
-    font-weight: 700;
-    &:focus{
-        outline: none;
-    }
-`;
-
-const ButtonSeparator = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`;
-
-const TextSeparator = styled.span`
-    color: #979797;
-    font-size: .5rem;
-    font-weight: 100;
-`;
-
-const HR = styled.hr`
-    border: 0;
-    border-bottom: 1px solid #D7D7D7;
-    width: 45%;
-`;
-
-const FacebookButton = Button.extend`
-    background-color: rgb(60, 82, 136);
 `;
 
 const RedirectWrapper = styled.div`
@@ -171,24 +113,24 @@ class ModalSection extends Component {
         let redirect = undefined;
         let header = null;
         if(this.props.onModalData === 'login'){
-            data = ['Log in', 'Forgot Password?', 'SIGN IN', 'New to my website?', 'SIGN UP'];
+            data = ['Log in', 'Forgot Password?', 'New to my website?', 'SIGN UP'];
             formTerms = <ForgotPasswordLink> {data[1]} </ForgotPasswordLink>
             if(this.state.width<=768){
-                redirect = <RedirectRouterLink to='/signup' onClick={this.props.redirectModal}> {data[4]} </RedirectRouterLink>;
+                redirect = <RedirectRouterLink to='/signup' onClick={this.props.redirectModal}> {data[3]} </RedirectRouterLink>;
                 header = <Header/>;
             }
             else {
-                redirect = <RedirectLink href="" onClick={this.props.redirectModal}> {data[4]} </RedirectLink>
+                redirect = <RedirectLink href="#" onClick={this.props.redirectModal}> {data[3]} </RedirectLink>
             }
         } else {
-            data = ['Sign up', 'By signing up, you agree to my Terms of Service.', 'SIGN UP', 'Already have an account?', 'LOG IN']
+            data = ['Sign up', 'By signing up, you agree to my Terms of Service.', 'Already have an account?', 'LOG IN']
             formTerms = <TermsText> {data[1]} </TermsText>
             if(this.state.width<=768){
-                redirect = <RedirectRouterLink to='/login' onClick={this.props.redirectModal}> {data[4]} </RedirectRouterLink>
+                redirect = <RedirectRouterLink to='/login' onClick={this.props.redirectModal}> {data[3]} </RedirectRouterLink>
                 header = <Header/>;
             }
             else {
-                redirect = <RedirectLink href="" onClick={this.props.redirectModal}> {data[4]} </RedirectLink>
+                redirect = <RedirectLink href="#" onClick={this.props.redirectModal}> {data[3]} </RedirectLink>
             }
         }
         return (
@@ -197,22 +139,11 @@ class ModalSection extends Component {
                 <Heading>
                     {data[0]}
                 </Heading>
-                <Form>
-                    <Input placeholder='Email address'/>
-                    <Input placeholder='Password'/>
+                <Form onModalForm={this.props.onModalData}>
                     {formTerms}
-                    <Button> {data[2]} </Button>
-                    <ButtonSeparator>
-                        <HR/>
-                        <TextSeparator>
-                            or
-                        </TextSeparator>
-                        <HR/>
-                    </ButtonSeparator>
-                    <FacebookButton> FACEBOOK </FacebookButton>
                 </Form>
                 <RedirectWrapper>
-                    <RedirectText> {data[3]} </RedirectText>
+                    <RedirectText> {data[2]} </RedirectText>
                     {redirect}
                 </RedirectWrapper>
                 <CloseButton onClick={this.onToggleModal.bind(this)}/>
